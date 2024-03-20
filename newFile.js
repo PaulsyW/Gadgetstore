32c0540d2770fb23155b5962f2b573aca364c979
paulsyW <paulsyw3@gmail.com>
2024-03-20 18:11:47 +0300

    createicons

---
+++ newFile.js
@@ -0,0 +1,9 @@
+ function showPopup(item) {
+     var popup = item.querySelector('.shop-item-popup');
+     popup.style.display = 'block';
+ }
+
+ function hidePopup(item) {
+     var popup = item.querySelector('.shop-item-popup');
+     popup.style.display = 'none';
+ }
